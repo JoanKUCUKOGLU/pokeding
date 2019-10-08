ed794b57263648e29c97ae1a29e8f09659c51195
Unknown <yanis88357@gmail.com>
2019-10-08 16:04:38 +0200

    Pokemon class

--- listedepokemon/Controlers/ViewController.swift
+++ listedepokemon/Controlers/ViewController.swift
@@ -15,10 +15,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        let pkmn = Pokemon();
+        pkmn.setPkmn(id: "557a723880a20c9db3bc31c2", pkmnId: 1, imgUrl: "http://assets22.pokemon.com/assets/cms2/img/pokedex/full/001.png", desc: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.  Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.", types: "grass", evol: "557a723880a20c9db3bc31c3")
         
     }
     
 }
+
 
 
---
+++ listedepokemon/Models/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  listedepokemon
+//
+//  Created by Verbeke Yanis on 08/10/2019.
+//  Copyright © 2019 Kucukoglu Joan. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    var id : String = ""
+    var pkmnId : Int = 0
+    var imgUrl : String = ""
+    var description : String = ""
+    var types : String = ""
+    var evolutions : String = ""
+    
+    func setPkmn(id : String, pkmnId : Int, imgUrl : String, desc : String, types : String, evol : String) {
+        self.id = id
+        self.pkmnId = pkmnId
+        self.imgUrl = imgUrl
+        self.description = desc
+        self.types = types
+        self.evolutions = evol
+    }
+}
